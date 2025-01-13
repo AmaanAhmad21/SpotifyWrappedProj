251129aee00969a1a9fb7bb6e06738e3ae8b9671
AmaanAhmad21 <amaanzahmad2004@gmail.com>
2025-01-12 23:11:00 -0500

    Got the authorization to work and displays tracks on the stats page.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, url_for, session, redirect, render_template
+from flask import Flask, request, url_for, session, redirect
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 from dotenv import load_dotenv
@@ -7,25 +7,53 @@ import os
 # Load environment variables from .env file
 load_dotenv()
 
-# Access constants
+# Constants
 CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID")
 CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET")
+TOKEN_INFO = "token_info"
 
+# Set the secret key for session encryption
 app = Flask(__name__)
+app.secret_key = os.getenv("SPOTIFY_CLIENT_SECRET")  # Ensure it's set
 
-# Create all the routes.
+def createSpotifyOAuth():
+    return SpotifyOAuth(
+        client_id=CLIENT_ID,
+        client_secret=CLIENT_SECRET,
+        redirect_uri=url_for("redirect_page", _external=True),
+        scope="user-top-read"
+    )
+
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
 
 @app.route("/login")
 def login():
-    return "<p>Hello, World!</p>"
+    sp_oauth = createSpotifyOAuth()
+    auth_url = sp_oauth.get_authorize_url()
+    return redirect(auth_url)
 
-@app.route("/redirect")
-def redirect():
-    return "<p>Hello, World!</p>"
+
+@app.route("/redirect_page")
+def redirect_page():
+    code = request.args.get("code")
+    sp_oauth = createSpotifyOAuth()
+    token_info = sp_oauth.get_access_token(code)
+    session[TOKEN_INFO] = token_info
+    return redirect(url_for("stats", _external=True))
+
+def getToken():
+    token_info = session.get(TOKEN_INFO, None)
+    return token_info
 
 @app.route("/stats")
 def stats():
-    return "<p>Hello, World!</p>"
+    user_token = getToken()
+    if not user_token:
+        print("No token, redirecting to login...")  # Debugging statement
+        return redirect(url_for("login"))
+
+    sp = spotipy.Spotify(auth=user_token['access_token'])
+    userTopSongs = sp.current_user_top_tracks(limit=5, offset=0, time_range="short_term")
+    return str(userTopSongs['items'])
