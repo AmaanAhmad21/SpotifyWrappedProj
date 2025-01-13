82a166ff35867dd772c92bac88b7e6e8fc8720ca
AmaanAhmad21 <amaanzahmad2004@gmail.com>
2025-01-12 21:24:08 -0500

    Move secrets to environment file

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 from flask import Flask, request, url_for, session, redirect, render_template
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Access constants
+CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID")
+CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET")
 
 app = Flask(__name__)
 
