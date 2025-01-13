0139dbd4661400bdad2b698090dc177c26d5cb79
AmaanAhmad21 <amaanzahmad2004@gmail.com>
2025-01-12 21:15:58 -0500

    Imported Spotipy

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask
+from flask import Flask, request, url_for, session, redirect, render_template
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
 
 app = Flask(__name__)
 
-#Create all the routes.
+# Create all the routes.
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
